38d10cb68f9eae13253d67deb062e894079551ea
JANG JI YONG <zdy2010@naver.com>
2024-12-12 12:58:39 +0900

    feat: MainView 추가

--- app/src/main/java/com/example/chian/MainActivity.kt
+++ app/src/main/java/com/example/chian/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.chian
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.chian.navigation.RootNavGraph
+import com.example.chian.navigation.Route
+import com.example.chian.navigation.bottomNavigation.BottomNavigationBar
 import com.example.chian.ui.theme.ChianTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    MainView(navController = navController)
                 }
             }
         }
@@ -30,17 +41,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainView(
+    navController: NavHostController
+) {
+    val navBackStackEntry = navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry.value?.destination?.route
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ChianTheme {
-        Greeting("Android")
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .navigationBarsPadding(),
+        bottomBar = {
+            if (currentRoute != Route.CHAT) {
+                BottomNavigationBar(navController = navController)
+            }
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            RootNavGraph(navHostController = navController)
+        }
     }
 }
