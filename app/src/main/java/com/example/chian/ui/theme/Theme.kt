8164faf06e0109cf7df6edc2bb9568c7115a7d4e
JANG JI YONG <zdy2010@naver.com>
2024-12-13 01:50:03 +0900

    feat: 상태방 White 고정

--- app/src/main/java/com/example/chian/ui/theme/Theme.kt
+++ app/src/main/java/com/example/chian/ui/theme/Theme.kt
@@ -1,6 +1,7 @@
 package com.example.chian.ui.theme
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -57,7 +58,7 @@ fun ChianTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = Color.WHITE
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
