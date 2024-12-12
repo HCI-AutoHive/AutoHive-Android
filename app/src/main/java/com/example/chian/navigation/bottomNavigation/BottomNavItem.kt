c608143672255df4686e072fce502df0cbfa117f
JANG JI YONG <zdy2010@naver.com>
2024-12-12 12:54:36 +0900

    feat: BottomNavigation 추가

---
+++ app/src/main/java/com/example/chian/navigation/bottomNavigation/BottomNavItem.kt
@@ -0,0 +1,64 @@
+package com.example.chian.navigation.bottomNavigation
+
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.example.chian.R
+import com.example.chian.navigation.Route
+import com.example.chian.ui.theme.iconSelctedColor
+import com.example.chian.ui.theme.iconUnselectedColor
+
+sealed class BottomNavItem(
+    val route: String,
+    val title: String,
+    val icon: @Composable (Boolean) -> Unit
+) {
+    object ChatScreen : BottomNavItem(Route.CHAT, "챗봇", { isSelect ->
+        if (isSelect) {
+            Icon(
+               imageVector = ImageVector.vectorResource(id = R.drawable.ic_chat),
+                contentDescription = "챗봇 아이콘",
+                tint = iconSelctedColor
+            )
+        } else {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_chat),
+                contentDescription = "챗봇 아이콘",
+                tint = iconUnselectedColor
+            )
+        }
+    })
+
+    object HomeScreen : BottomNavItem(Route.HOME, "홈", { isSelect ->
+        if (isSelect) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_home),
+                contentDescription = "리뷰 아이콘",
+                tint = iconSelctedColor
+            )
+        } else {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_home),
+                contentDescription = "리뷰 아이콘",
+                tint = iconUnselectedColor
+            )
+        }
+    })
+
+    object ReviewMainScreen : BottomNavItem(Route.DETAILS_MAIN, "리뷰", { isSelect ->
+        if (isSelect) {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_stocks),
+                contentDescription = "홈 아이콘",
+                tint = iconSelctedColor
+            )
+        } else {
+            Icon(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_stocks),
+                contentDescription = "홈 아이콘",
+                tint = iconUnselectedColor
+            )
+        }
+    })
+}
