d947aad3353399b205aeaf25556258b75a3aa382
JANG JI YONG <zdy2010@naver.com>
2024-12-12 12:57:16 +0900

    feat: text 고도화 작업 및 내용 추가 및 수정

--- app/src/main/java/com/example/chian/screen/MainScreen.kt
+++ app/src/main/java/com/example/chian/screen/MainScreen.kt
@@ -8,12 +8,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -35,10 +35,10 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.chian.R
 import com.example.chian.component.ChianTopBar
-import com.example.chian.ui.theme.mainBlueColor
+import com.example.chian.ui.theme.chatBoxAccentTextColor
+import com.example.chian.ui.theme.chatBoxBackColor
+import com.example.chian.ui.theme.mainColor
 import com.example.chian.ui.theme.mainHeadLineColor
-import com.example.chian.ui.theme.textFieldBackGroundColor
-import com.example.chian.ui.theme.textYellowColor
 
 @Composable
 fun MainScreen() {
@@ -59,14 +59,7 @@ fun MainScreen() {
                             fontSize = 24.sp
                         )
                     ) {
-                        append(text = "차량에 대해서 알아가기 어려우시죠?")
-                    }
-                    withStyle(
-                        SpanStyle(
-                            fontSize = 16.sp
-                        )
-                    ) {
-                        append(text = "\n저희 Chian을 통해 쉽게 알아가봐요.")
+                        append(text = "차량에 대해서 쉽게 알아봐요!")
                     }
                 },
                 style = TextStyle(
@@ -79,7 +72,17 @@ fun MainScreen() {
 
             Text(
                 modifier = Modifier.padding(bottom = 8.dp),
-                text = "Chian의 특징 및 사용 방법이 뭔가요?",
+                text = buildAnnotatedString {
+                    withStyle(
+                        SpanStyle(
+                            fontSize = 17.sp,
+                            color = mainColor
+                        )
+                    ) {
+                        append("Chian")
+                    }
+                    append("의 특징 및 사용 방법이 뭔가요?")
+                },
                 style = TextStyle(
                     fontFamily = FontFamily(Font(R.font.nanum_square_b)),
                     color = mainHeadLineColor,
@@ -88,19 +91,58 @@ fun MainScreen() {
             )
             Box(
                 modifier = Modifier
-                    .border(1.dp, Color.Red, RoundedCornerShape(12.dp))
-                    .padding(8.dp)
+                    .border(1.dp, mainColor, RoundedCornerShape(12.dp))
+                    .padding(18.dp)
             ) {
                 Column {
                     Text(
-                        text = "저희 Chian은 AI 기반 + RAG 시스템을 활용한 챗봇을 통해 자동차의 성능에 대한 정보를 제공해요." +
-                                "\n일일이 하나씩 찾아 들어가지 않아도, 챗봇을 통해 빠르게 정보를 얻을 수 있어요.\n" +
-                                "처음 쓰시는 분도 아래 예시와 같이 사용하시면 편하게 이용 가능해요! " +
-                                "\n같이 아래로 이동해서 사용 방법을 볼까요?",
+                        text = buildAnnotatedString {
+                            append("저희는 ")
+                            withStyle(
+                                SpanStyle(
+                                    fontSize = 14.sp,
+                                    color = chatBoxAccentTextColor
+                                )
+                            ) {
+                                append("RAG ")
+                            }
+                            append("시스템을 활용한 챗봇을 통해 자동차의 성능에 대한 정보를 제공해요.\n\n")
+                            append("일일이 하나씩 찾아 들어가지 않아도, ")
+                            withStyle(
+                                SpanStyle(
+                                    fontSize = 14.sp,
+                                    color = chatBoxAccentTextColor
+                                )
+                            ) {
+                                append("챗봇")
+                            }
+                            append("을 통해 빠르게 정보를 얻을 수 있어요. \n\n")
+                            append("처음 쓰시는 분도 ")
+                            withStyle(
+                                SpanStyle(
+                                    fontSize = 14.sp,
+                                    color = chatBoxAccentTextColor
+                                )
+                            ) {
+                                append("아래 예시")
+                            }
+                            append("와 같이 사용하시면 편하게 이용 가능해요! \n\n")
+                            append("같이 ")
+                            withStyle(
+                                SpanStyle(
+                                    fontSize = 14.sp,
+                                    color = chatBoxAccentTextColor
+                                )
+                            ) {
+                                append("아래로 이동해서 ")
+                            }
+                            append("사용 방법을 볼까요?")
+                        },
                         style = TextStyle(
                             fontFamily = FontFamily(Font(R.font.nanum_square_r)),
                             color = Color.Black,
-                            fontSize = 13.sp
+                            fontSize = 13.sp,
+                            lineHeight = 16.sp
                         )
                     )
                 }
@@ -110,7 +152,17 @@ fun MainScreen() {
 
             Text(
                 modifier = Modifier.padding(bottom = 8.dp),
-                text = "Chian 사용 방법",
+                text = buildAnnotatedString {
+                    withStyle(
+                        SpanStyle(
+                            fontSize = 17.sp,
+                            color = mainColor
+                        )
+                    ) {
+                        append("Chian")
+                    }
+                    append("의 사용 방법")
+                },
                 style = TextStyle(
                     fontFamily = FontFamily(Font(R.font.nanum_square_b)),
                     color = mainHeadLineColor,
@@ -124,7 +176,7 @@ fun MainScreen() {
             )
 
             Row(
-                modifier = Modifier.padding(start = 10.dp,top = 5.dp),
+                modifier = Modifier.padding(start = 10.dp, top = 5.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
@@ -143,6 +195,8 @@ fun MainScreen() {
                 )
             }
 
+            Spacer(modifier = Modifier.height(20.dp))
+
             Text(
                 modifier = Modifier.fillMaxWidth(),
                 text = buildAnnotatedString {
@@ -156,7 +210,7 @@ fun MainScreen() {
                     withStyle(
                         SpanStyle(
                             fontSize = 18.sp,
-                            color = mainBlueColor,
+                            color = mainColor,
                             fontFamily = FontFamily(Font(R.font.nanum_square_b))
                         )
                     ) {
@@ -188,8 +242,8 @@ private fun ChatBotBox(
     Box(
         modifier = modifier
             .clip(RoundedCornerShape(12.dp))
-            .border(1.dp, mainBlueColor, RoundedCornerShape(12.dp))
-            .background(textFieldBackGroundColor)
+            .border(1.dp, mainColor, RoundedCornerShape(12.dp))
+            .background(chatBoxBackColor)
     ) {
         Column(
             modifier = Modifier,
@@ -227,13 +281,13 @@ private fun ChatBotBox(
                     Text(
                         text = buildAnnotatedString {
                             append("AI 기반의 ")
-                            withStyle(SpanStyle(color = mainBlueColor)) { append("챗봇") }
+                            withStyle(SpanStyle(chatBoxAccentTextColor)) { append("챗봇") }
                             append("에게 빠르게 자동차의 ")
-                            withStyle(SpanStyle(color = mainBlueColor)) { append("성능") }
+                            withStyle(SpanStyle(chatBoxAccentTextColor)) { append("성능") }
                             append("에 대해서 ")
-                            withStyle(SpanStyle(color = mainBlueColor)) { append("대화") }
+                            withStyle(SpanStyle(chatBoxAccentTextColor)) { append("대화") }
                             append("를 해보실래요?\n\n")
-                            withStyle(SpanStyle(color = mainBlueColor)) { append("차량명(모델명)") }
+                            withStyle(SpanStyle(chatBoxAccentTextColor)) { append("차량명(모델명)") }
                             append("만 작성해 주시면 됩니다.")
                         },
                         style = TextStyle(
@@ -262,10 +316,10 @@ private fun ChatBotBox(
                                 bottomEnd = 12.dp
                             )
                         )
-                        .background(mainBlueColor)
+                        .background(mainColor)
                         .border(
                             1.dp,
-                            mainBlueColor,
+                            mainColor,
                             RoundedCornerShape(
                                 topStart = 12.dp,
                                 topEnd = 2.dp,
@@ -279,7 +333,7 @@ private fun ChatBotBox(
                         text = "쏘나타 DN8",
                         style = TextStyle(
                             fontFamily = FontFamily(Font(R.font.nanum_square_r)),
-                            color = textYellowColor,
+                            color = Color(0xFF4C221A),
                             fontSize = 14.sp
                         )
                     )
@@ -317,11 +371,11 @@ private fun ChatBotBox(
                 ) {
                     Text(
                         text = buildAnnotatedString {
-                            withStyle(SpanStyle(color = mainBlueColor)) { append("1. 평점: ") }
+                            withStyle(SpanStyle(chatBoxAccentTextColor)) { append("1. 평점: ") }
                             append("4.0/5\n\n")
-                            withStyle(SpanStyle(color = mainBlueColor)) { append("2. 성능: ") }
+                            withStyle(SpanStyle(chatBoxAccentTextColor)) { append("2. 성능: ") }
                             append("쏘나타 DN8의 성능은 ...\n\n")
-                            withStyle(SpanStyle(color = mainBlueColor)) { append("3. 안전성: ") }
+                            withStyle(SpanStyle(chatBoxAccentTextColor)) { append("3. 안전성: ") }
                             append(" 쏘나타 DN8은 견고한 플랫폼 설계와 ... ")
                         },
                         style = TextStyle(
@@ -357,7 +411,7 @@ private fun ChatBotBox(
                         modifier = Modifier
                             .width(78.dp)
                             .fillMaxHeight()
-                            .background(mainBlueColor)
+                            .background(mainColor)
                             .clickable {
                                 onClickListener()
                             }
