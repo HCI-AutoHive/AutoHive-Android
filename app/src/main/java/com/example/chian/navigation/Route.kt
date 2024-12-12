5ed74a62e224a5f02edd484a932f5d14e6edd72f
JANG JI YONG <zdy2010@naver.com>
2024-12-12 12:58:24 +0900

    feat: Route 이름 정리하는 오브젝트 추가

---
+++ app/src/main/java/com/example/chian/navigation/Route.kt
@@ -0,0 +1,8 @@
+package com.example.chian.navigation
+
+object Route {
+    const val HOME = "home"
+    const val CHAT = "chat"
+    const val DETAILS = "details"
+    const val DETAILS_MAIN = "detailsMain"
+}
