39d5c002ed1d3d3c51c90b43ef77188307963a53
JANG JI YONG <zdy2010@naver.com>
2024-12-17 14:18:08 +0900

    feat: 이미지 및 URL을 데이터 클래스에서 받아오게 추가

--- app/src/main/java/com/example/chian/screen/DetailsMainScreen.kt
+++ app/src/main/java/com/example/chian/screen/DetailsMainScreen.kt
@@ -1,6 +1,8 @@
 package com.example.chian.screen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -22,6 +24,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.SpanStyle
@@ -33,10 +36,13 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import com.example.chian.R
 import com.example.chian.component.ChianTopBar
+import com.example.chian.ui.theme.chatBoxBackColor
+import com.example.chian.ui.theme.mainColor
 import com.example.chian.viewmodel.MainViewModel
 
 @Composable
@@ -44,6 +50,8 @@ fun DetailsMainScreen(
     mainViewModel: MainViewModel,
     navHostController: NavHostController
 ) {
+    val carsData = mainViewModel.carsData.collectAsStateWithLifecycle()
+
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -67,13 +75,26 @@ fun DetailsMainScreen(
                 .fillMaxSize()
                 .background(Color.White)
         ) {
-            items(10) {
-                DetailBox()
+            items(carsData.value.size) {
+                DetailBox(
+                    modifier = Modifier.clickable {
+                        mainViewModel.getCurrentCarId(carsData.value[it].carId)
+                        mainViewModel.getCarDetailData()
+                        mainViewModel.gutterCarNameImageData(
+                            carsData.value[it].model,
+                            carsData.value[it].imageUrl
+                        )
+                        navHostController.navigate("details")
+                    },
+                    carName = carsData.value[it].model,
+                    star = carsData.value[it].starRating,
+                    imageUrl = carsData.value[it].imageUrl
+                )
                 Spacer(
                     modifier = Modifier
                         .height(1.dp)
                         .fillMaxWidth()
-                        .background(Color.LightGray)
+                        .background(chatBoxBackColor)
                 )
             }
         }
@@ -81,23 +102,30 @@ fun DetailsMainScreen(
 }
 
 @Composable
-fun DetailBox() {
+fun DetailBox(
+    modifier: Modifier,
+    carName: String,
+    star: Double,
+    imageUrl: String
+) {
     Row(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .padding(18.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
-        //verticalAlignment = Alignment.CenterVertically
-    ){
+    ) {
         Box(
             modifier = Modifier
                 .size(100.dp)
                 .clip(RoundedCornerShape(12.dp))
-                .background(Color.Red)
-        ){
+                .background(Color.Transparent)
+                .border(1.dp, mainColor, RoundedCornerShape(12.dp))
+        ) {
             AsyncImage(
-                model = "",
-                contentDescription = "차량 이미지"
+                modifier = Modifier.fillMaxSize(),
+                model = imageUrl,
+                contentDescription = "차량 이미지",
+                contentScale = ContentScale.Inside
             )
         }
 
@@ -106,15 +134,20 @@ fun DetailBox() {
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             Text(
-                text = "현대 쏘나타 디 엣지(DN8) 가솔린 2L(23년~24년)",
+                text = carName,
                 style = TextStyle(
                     fontFamily = FontFamily(Font(R.font.nanum_square_b)),
                     fontSize = 15.sp
                 )
             )
-
+            /* 종합 평점 : $star / 5 */
             Text(
-                text = "종합 평점 : 4.5",
+                text = buildAnnotatedString {
+                    withStyle(SpanStyle(color = mainColor)) {
+                        append("종합 평점")
+                    }
+                    append(" : $star / 5")
+                },
                 style = TextStyle(
                     fontFamily = FontFamily(Font(R.font.nanum_square_b)),
                     fontSize = 15.sp
